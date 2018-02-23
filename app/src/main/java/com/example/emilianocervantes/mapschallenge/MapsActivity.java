1c3e1a7d4bd9dc38e3be5782287edddcaac68e82
Emiliano Cervantes <ovman96@hotmail.com>
2018-02-23 13:00:23 -0600

    Map restaurantes cdmx with Google API places

--- app/src/main/java/com/example/emilianocervantes/mapschallenge/MapsActivity.java
+++ app/src/main/java/com/example/emilianocervantes/mapschallenge/MapsActivity.java
@@ -5,13 +5,20 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
+import android.os.MessageQueue;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -21,9 +28,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
@@ -39,6 +54,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Marker marker;
     //puede ser cualquier numero
     private final int REQUEST_LOCATION_CODE = 69;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +68,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    public void execute(View view){
+        switch (view.getId()){
+            case R.id.zoomIn:
+                mMap.animateCamera(CameraUpdateFactory.zoomIn());
+                break;
+            case R.id.zoomOut:
+                mMap.animateCamera(CameraUpdateFactory.zoomOut());
+                break;
+            case R.id.style:
+                if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL){
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                } else if(mMap.getMapType() == GoogleMap.MAP_TYPE_SATELLITE){
+                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                } else if (mMap.getMapType() == GoogleMap.MAP_TYPE_HYBRID) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                }else {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+                break;
+            case R.id.challenge:
+                jsonMap("https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+CDMX&key=AIzaSyB2vj6e3SsNUFPhGCEF9dazLKMWLAKYP3c");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void jsonMap(String url){
+        //Limpie el adapatador
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("results");
+                    for(int i = 0; i < jsonArray.length();i++){
+                        JSONObject jsonObject01 = jsonArray.getJSONObject(i);
+                        JSONObject gemoetry = jsonObject01.getJSONObject("geometry");
+                        JSONObject location = gemoetry.getJSONObject("location");
+                        double lat = location.getDouble("lat");
+                        double lon = location.getDouble("lng");
+                        LatLng cdmxRestaurantes = new LatLng(lat, lon);
+                        mMap.addMarker(new MarkerOptions().position(cdmxRestaurantes).title("Restaurante"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(cdmxRestaurantes));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //
+            }
+        });
+        mQueue.add(request);
     }
 
     /**
@@ -101,18 +176,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .addApi(LocationServices.API)
                     .build();
             googleApiClient.connect();
+            //Habilitar la parte de mapa
+            //Da un icono dentro del mapa para localizacion
+            mMap.setMyLocationEnabled(true);
         }
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
     }
 
     //Donde esta ubicado actualmente
     @Override
     public void onLocationChanged(Location location) {
-
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+        this.location = location;
+        if(marker != null){
+            marker.remove();
+        }
+        LatLng latLng = new LatLng(lat, lon);
+        //Generar el marcador
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng).title("Aquí merocles")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+        this.marker = mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //Animar la camara
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+        //Para que deje de actualizar y hacer el flicker (acercamiento cada ciertos segundos)
+        if(googleApiClient != null){
+            LocationServices.FusedLocationApi
+                    .removeLocationUpdates(googleApiClient,this);
+        }
     }
 
     //Cuando ya esta conectado al servicio de localizacion
